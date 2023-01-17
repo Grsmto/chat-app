6caca8285be7971386bfd4a009c87370288c8dd9
Adrien Denat <oi@adriendenat.com>
2023-01-17 22:16:10 +0000

    add MessageInput component

--- src/app/chats/[slug]/page.tsx
+++ src/app/chats/[slug]/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { MessageInput } from "@/components/MessageInput";
+
 export default function Page({ params }: { params: { slug: string } }) {
+  const handleSubmit = (value: string) => {
+    console.log(value);
+  };
+
   return (
     <main className="flex flex-col flex-auto h-full w-full p-6">
       <div className="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
@@ -9,6 +15,7 @@ export default function Page({ params }: { params: { slug: string } }) {
             <div className="grid grid-cols-12 gap-y-2"></div>
           </div>
         </div>
+        <MessageInput onSend={handleSubmit} />
       </div>
     </main>
   );
---
+++ src/components/MessageInput/index.tsx
@@ -0,0 +1,52 @@
+import {
+  ChangeEvent,
+  FormEvent,
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+
+type MessageInputProps = {
+  onSend: (text: string) => any;
+};
+
+export const MessageInput = ({
+  onSend,
+}: PropsWithChildren<MessageInputProps>) => {
+  const [value, setValue] = useState<string>("");
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    onSend(value);
+    setValue("");
+  };
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.currentTarget.value);
+  }, []);
+
+  return (
+    <form
+      className="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex-grow">
+        <div className="relative w-full">
+          <input
+            autoFocus
+            type="text"
+            className="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+            onChange={handleChange}
+            value={value}
+          />
+        </div>
+      </div>
+      <div className="ml-4">
+        <button className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+          Send
+        </button>
+      </div>
+    </form>
+  );
+};
